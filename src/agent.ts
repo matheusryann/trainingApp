21132d921c5cf52a3f1a06967a68302ecce9ea41
Matheus <matheus.ryan@discente.ufma.br>
2025-09-26 15:34:27 -0300

    feat: configure headers of server-sent-events on plan route

--- src/agent.ts
+++ src/agent.ts
@@ -1,12 +1,40 @@
 import OpenAI from "openai";
+import fs from "fs";
 
 import {buildDocsSystemPrompt, buildSystemPrompt, buildUserPrompt} from './prompt';
 
 import type { createPlanSchemaType } from "./types";
 
 
-export async function generatePlan(input: createPlanSchemaType) {
-  console.log(input)
 
-  return input;
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY as string,
+    timeout: 2 * 60 * 1000, // 2 minutes
+    logLevel: "debug"
+  });
+export async function* generatePlan(input: createPlanSchemaType) {
+  const diretrizes = fs.readFileSync("knowledge/diretrizes.md", "utf-8");
+   
+  const stream = await openai.chat.completions.create({
+    model: "gpt-4o-mini",
+    messages: [
+      {
+        role: "system",
+        content: buildSystemPrompt()
+      },
+      {
+        role: "user",
+        content: buildUserPrompt(input)
+      },
+    ],
+    temperature: 0.6, // the bigger the value, the more creative the response
+    stream: true,
+});
+
+
+for await (const chunk of stream) {
+  const delta = chunk.choices[0]?.delta?.content;
+  if(delta) yield delta;
+}
+
 }
--- src/routes/plan.ts
+++ src/routes/plan.ts
@@ -1,18 +1,31 @@
 
 import type { FastifyInstance } from "fastify";
 import { createPlanSchema} from "../types";
+import { generatePlan } from "../agent";
 
 export async function planRoutes(app: FastifyInstance) { 
     app.post("/plan", async (request, reply) => {
+      reply.raw.setHeader("Access-Control-Allow-Origin", "*");
+      reply.raw.setHeader("Content-Type", "text/plain; charset=utf-8");
+      reply.raw.setHeader("Content-Type", "text/event-stream");
+      reply.raw.setHeader("Cache-Control", "no-cache");
+      reply.raw.setHeader("Connection", "keep-alive");
         
       const parse = createPlanSchema.safeParse(request.body);
       if (!parse.success) {
-        return reply.status(400).send(parse.error);
+        return reply.status(400).send({
+          error: "Invalid request",
+          details: parse.error.flatten(issue => issue.message),
+        });
       }
       
       try {
         
+        for await (const chunk of generatePlan(parse.data)) {
+          reply.raw.write(chunk);
+        }
 
+        reply.raw.end();
 
 
       } catch (error: any) {
@@ -20,6 +33,8 @@ export async function planRoutes(app: FastifyInstance) {
         reply.raw.write(`event: error\n ${JSON.stringify(error.message)}\n\n`);
         reply.raw.end();
       }
+
+      return reply;
 
       });
     }
